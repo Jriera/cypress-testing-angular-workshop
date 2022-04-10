3e8d7a082d31dc53524d3b615b20d45ec65ab8c9
jriera <jordiriera81@gmail.com>
2022-04-10 13:06:30 +0200

    Activity3: Test using cy.contains and selectors

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,8 +1,20 @@
 /// <reference types="cypress"/>
 /// http://on.cypress.io/intellisense
 
-describe("localhost visited",() =>{
+describe("Workshop tests",() =>{
   it("should load localhost",()=>{
     cy.visit("");
+  })
+  it("should find the banner and assert its content",()=>{
+    cy.get('.banner > .container > .logo-font').contains("conduit")
+  })
+  it("should find second tab and assert its style",()=>{
+    cy.get('.feed-toggle > .nav > :nth-child(2) > .nav-link').should('have.css','color','rgb(92, 184, 92)')
+  })
+  it("should capture the styling class of the navbar",()=>{
+    cy.get(".navbar").should("have.class", "navbar-light");
+  })
+  it('should find a subtitle string without element selection',()=>{
+    cy.contains('A place to share your Angular knowledge.')
   })
 })
